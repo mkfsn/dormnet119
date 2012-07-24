547efac7d983fa5ed810b2d6bc7203f74476fd59
mkfsn <p408865@gmail.com>
2012-07-24 22:15:16 +0800

    create guestbook index file

---
+++ guestbook/index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Team2 Guestbook</title>
+</head>
+
+<body>
+<div id="head">
+    <table>
+        <tr>
+            <td>Name</td>
+            <td><input type="text" id="name" style="width: 100%;" ></td>
+        </tr>
+        <tr>
+            <td>Content</td>
+            <td><textarea id="content"></textarea></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td style="text-align:right;">
+                <input type="button" value="Send" id="send">
+                <input type="button" value="Clear" id="clear">
+            </td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
