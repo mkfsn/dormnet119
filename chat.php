5413954c11f011b04aee8b36d0798901806d974d
CDPAlolita <andywuv@yahoo.com.tw>
2012-07-24 22:21:08 +0800

    lolita_chat room

--- chat.php
+++ chat.php
@@ -1,111 +1,55 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
+
 <html>
-<body>
 
-<?php
-	// Function prototypes
-	
-	function func_write($user, $msg, $date)
-	{
-		$f = fopen("board.txt", "a+");
-		
-		$str = '<br />' . 	htmlspecialchars($user) .
-							' (' . $date . ') said : <br />' .
-							htmlspecialchars($msg);
-		fwrite($f, $str);
-		
-		fclose($f);
-	}
-	
-	function func_read_all()
-	{
-		$f = fopen("board.txt", "r+");
-		$rst = "";
-		
-		while ( !feof($f))
-		{
-			$line = fgets($f);
-			$line = $line;
-			$rst .= $line . "<br />";
-		}
-		
-		fclose($f);
-		
-		return $rst;
-	}
+<style type="text/css">
+body{
+	text-align: center;
+}
+</style>
 
-?>
+<h1>WE LOVE LOLI</h1>
+<div style =" background : pink;">
+<h1>讚美LOLITA</h1>
+</div>
+<br/>
+ <head><title>stay salt board</title></head>
+ <body bgcolor=06aaff >
+  <form action="chat.php" method="post">
+  <label class="mau">name:</label>
+  <input type ="text" name="fname"/><br/><br/>
+  <label>said:</label>
+  <textarea rows="10" cols="20" name="talk" >
+  </textarea>
+  
+  <tfoot>
+   <tr>
+    <td>
+     <input type="submit" value="Send"/>
+    </td>
+   </tr>
+  </tfoot>
 
-
-<?php
-	// Declaration
-	$username = "";
-	
-	session_start();
-	
-	// Get clear action
-	
-	
-	// Get post info.
-	if (isset($_POST['username']) && isset($_POST['messages']))
-	{
-		$tmp_user = $_POST['username'];
-		$tmp_msg  = $_POST['messages'];
-		$tmp_date = date("Y-m-d H:i:s");
-		
-		func_write($tmp_user, $tmp_msg, $tmp_date);
-		
-		$_SESSION['username'] = $tmp_user;
-	}
-	
-	// Get user information
-	if (isset($_SESSION['username']))
-	{
-		$username = $_SESSION['username'];
-	}
-
-?>
-
-
-
-<h1>It's a fucking chat room</h1>
-
-
-
-<form action="chat.php" method="POST">
-	<!-- Name field -->
-	<br />
-	Name : <br />
-	<input type="text" name="username" size="50" value="<?php if ("" != $username) echo $username; ?>"
-	/>
-
-	<!-- Message field -->
-	<br /><br />
-	Messages : <br />
-	<textarea name="messages" cols="80" rows="10" style="font-size:18px" onfocus="this.select()"></textarea>
-
-	<!-- Submit button -->
-	<br /><br />
-	<input type="submit" name="submit" value="Submit" />
-</form>
-
-<form action="chat.php" method="POST">
-	
-
-	<!-- Clear button -->
-	<br /><br />
-	<input type="submit" name="clear" value="Clear" />
-</form>
-
-
-<!-- Messages -->
-<?php
-	
-	print(func_read_all());
-
-?>
-
-<br /><br />
-
-</body>
+<img src="http://i.imgur.com/gmFL4.jpg">
+<img src="http://i.imgur.com/Dq4a0.gif"> 
+  <br/> 
+ </body>
 </html>
 
+<?php
+if($_POST['talk'] != NULL || $_POST['frame'] != NULL)
+{
+  $q = fopen("remember.txt","a+");
+  $sa = $_POST['talk'];
+  $na = $_POST['fname'];
+  fprintf($q,"%s: %s\n",$na,$sa);
+  fclose($q);
+}
+
+$q = fopen("remember.txt","r");
+while( $content = fgets($q) ) {
+	echo nl2br(htmlspecialchars($content));
+}
+fclose($q);
+
+?>
