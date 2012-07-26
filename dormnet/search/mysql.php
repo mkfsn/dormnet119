aa6b1dbd23a7f258b2f0e947a806f66fa89129e7
sTinGe <stinge@ubuntu.(none)>
2012-07-27 03:36:55 +0800

    images

--- dormnet/search/mysql.php
+++ dormnet/search/mysql.php
@@ -4,7 +4,7 @@
 	$password = 'team2GuestPass';
 
 	try {
-    		$dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    		$dbh = new PDO($dsn, $user, $password);
 	} 
 	catch (PDOException $e) {
     		echo 'Connection failed: ' . $e->getMessage();
